f6a57a286a66411450dcd79b462522d008d2dcd0
mpastusiak@live.com <mpastusiak@live.com>
2019-03-30 21:29:11 +0100

    19.2

--- src/main/java/com/crud/tasks/repository/TaskRepository.java
+++ src/main/java/com/crud/tasks/repository/TaskRepository.java
@@ -1,11 +1,16 @@
 package com.crud.tasks.repository;
 
 import com.crud.tasks.domain.Task;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface TaskRepository extends CrudRepository<Task, Long> {
     @Override
     List<Task> findAll();
+
+    @Query
+    Task findTaskById(@Param("id") long taskId);
 }
--- src/main/java/com/crud/tasks/service/DbService.java
+++ src/main/java/com/crud/tasks/service/DbService.java
@@ -15,4 +15,8 @@ public class DbService {
     public List<Task> getAllTasks() {
         return repository.findAll();
     }
+
+    public Task getTaskById(long taskId) {
+        return repository.findTaskById(taskId);
+    }
 }
