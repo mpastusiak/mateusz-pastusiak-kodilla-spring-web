68e6d89d1f4bc19a97afd076138a75f551ea5c5a
mpastusiak@live.com <mpastusiak@live.com>
2019-07-13 20:44:14 +0200

    JAR and PostgreSQL fixed

--- src/main/java/com/crud/tasks/domain/Mail.java
+++ src/main/java/com/crud/tasks/domain/Mail.java
@@ -2,6 +2,10 @@ package com.crud.tasks.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 @Getter
 @AllArgsConstructor
@@ -9,4 +13,5 @@ public class Mail {
     private String mailTo;
     private String subject;
     private String message;
+    private String toCc;
 }
